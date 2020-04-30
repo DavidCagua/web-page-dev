ead6e84cafc16b7eae074ea5a4517ae30dbbc607
David <felipe0160@gmail.com>
2020-04-30 16:00:48 -0500

    primer dia

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './pages/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
@@ -10,6 +9,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+if (module.hot) { module.hot.accept();}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Home extends Component {
+    render() {
+      return (
+        <div className="Home">
+            <p>Hola mundo </p>
+            <Link></Link>
+        </div>
+      );
+    }
+  }
