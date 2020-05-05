dace671e072b4f332eddf409f0d9d5f1e5267ab6
David <felipe0160@gmail.com>
2020-05-05 11:49:57 -0500

    Updates

---
+++ precache-manifest.7aa21cc4842cfe83c0465b16022eb9a5.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb4a74adc12cfbad996e52e6b215027",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d86c06d0bd003829583",
+    "url": "/static/css/main.d35c9eb1.chunk.css"
+  },
+  {
+    "revision": "6827da4ac12eb07996d0",
+    "url": "/static/js/2.29ebbc53.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.29ebbc53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d86c06d0bd003829583",
+    "url": "/static/js/main.d6cdd211.chunk.js"
+  },
+  {
+    "revision": "93f3b855e12990d11897",
+    "url": "/static/js/runtime-main.f0cd50dd.js"
+  },
+  {
+    "revision": "502a8acfd5bfe702c55294dd428c8a7b",
+    "url": "/static/media/006-like.502a8acf.png"
+  },
+  {
+    "revision": "90aab6ac03dc3ac2a9a5a4a880eff042",
+    "url": "/static/media/013-newsletter.90aab6ac.png"
+  },
+  {
+    "revision": "c93aea0e14ae5cab6dcfd2780927ff34",
+    "url": "/static/media/ProfilePic.c93aea0e.jpg"
+  },
+  {
+    "revision": "abfc3b93baa0cb512723b5517b79e695",
+    "url": "/static/media/angular-icon.abfc3b93.svg"
+  },
+  {
+    "revision": "55338d57e6f6c200bcd23f0af4710a9b",
+    "url": "/static/media/css.55338d57.svg"
+  },
+  {
+    "revision": "0000a414677dfc8df19cea9cf663d223",
+    "url": "/static/media/fabian-grohs-oZ61KFUQsus-unsplash.0000a414.jpg"
+  },
+  {
+    "revision": "94cf2a7a6f63496f0e3179806b80193c",
+    "url": "/static/media/html.94cf2a7a.svg"
+  },
+  {
+    "revision": "e169ba293cabaff0dc96397aed47a250",
+    "url": "/static/media/javascript.e169ba29.svg"
+  },
+  {
+    "revision": "4b9d67074bdea08052fb1146e4779e9d",
+    "url": "/static/media/linkedin.4b9d6707.svg"
+  },
+  {
+    "revision": "b018dd4ee2594a6b20da9b88e21dd5e5",
+    "url": "/static/media/mysql.b018dd4e.svg"
+  },
+  {
+    "revision": "be2df79601b32143d336bd076429b909",
+    "url": "/static/media/node.be2df796.svg"
+  },
+  {
+    "revision": "d956cbbb2e59777272370c854a3b6747",
+    "url": "/static/media/php.d956cbbb.svg"
+  },
+  {
+    "revision": "5911582b4e69992380b70634d86358f5",
+    "url": "/static/media/social-media.5911582b.svg"
+  },
+  {
+    "revision": "815f723ef69e9f8c43007b8712a66abe",
+    "url": "/static/media/twitter.815f723e.svg"
+  }
+]);
